d465bcbc5f0772059bbebcbf95af31fc80e0f3eb
nicholasas1 <nicholasantonius46@gmail.com>
2022-08-19 14:07:20 +0700

    Login slesai nih ehheheh

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
 import "./App.css";
 import { Link } from "react-router-dom";
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+const { useState } = React;
 
-function App() {
-  var state = "hai";
-  function hai(){
-    alert("hai");
-    state = "jauh";
-    fetch("https://jsonplaceholder.typicode.com/posts?_limit=10")
+const App = () => {
+  const [username, setValue] = useState("");
+  const handleChange = (e) => setValue(e.target.value);
+  const [password, setData] = useState("");
+  const handleChange2 = (e) => setData(e.target.value);
+  const [data, setResponse] = useState("");
+
+  const hai = () => {
+    fetch(
+      "https://api.papsarnoldus.my.id/login.php?username=" +
+        username +
+        "&password=" +
+        password
+    )
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((data) => checkLogin(data));
   };
 
-
+  const checkLogin = (e) => {
+    if (e.status == "eror") {
+      toast("Login Gagal");
+    } else if (e.status == "succes") {
+      if (e.result[0].Role == "Admin") {
+        window.location.href = "/dashboard";
+      } else {
+        toast("Hanya admin yang dapat login");
+      }
+    }
+  };
   return (
     <div className="App">
-      <button onClick={hai}>Hai</button>
+      <ToastContainer />
       <div class="hold-transition login-page">
         <div class="login-box">
           <div class="login-logo">
@@ -26,44 +47,42 @@ function App() {
           <div class="card">
             <div class="card-body login-card-body">
               <p class="login-box-msg">Sign in to start your session</p>
-
-              <form action="../../index3.html" method="post">
-                <div class="input-group mb-3">
-                  <input
-                    type="email"
-                    class="form-control"
-                    placeholder="Email"
-                    value={state}
-                  />
-                  <b>{state}</b>
-                  <div class="input-group-append">
-                    <div class="input-group-text">
-                      <span class="fas fa-envelope"></span>
-                    </div>
+              <div class="input-group mb-3">
+                <input
+                  type="username"
+                  class="form-control"
+                  placeholder="Email"
+                  value={username}
+                  onChange={handleChange}
+                />
+                <div class="input-group-append">
+                  <div class="input-group-text">
+                    <span class="fas fa-envelope"></span>
                   </div>
                 </div>
-                <div class="input-group mb-3">
-                  <input
-                    type="password"
-                    class="form-control"
-                    placeholder="Password"
-                  />
-                  <div class="input-group-append">
-                    <div class="input-group-text">
-                      <span class="fas fa-lock"></span>
-                    </div>
+              </div>
+              <div class="input-group mb-3">
+                <input
+                  type="password"
+                  class="form-control"
+                  placeholder="Password"
+                  value={password}
+                  onChange={handleChange2}
+                />
+                <div class="input-group-append">
+                  <div class="input-group-text">
+                    <span class="fas fa-lock"></span>
                   </div>
                 </div>
-                <div class="row">
-                  <div class="col-8">
-                  </div>
-                  <div class="col-4">
-                    <button type="submit" class="btn btn-primary btn-block">
-                      Sign In
-                    </button>
-                  </div>
+              </div>
+              <div class="row">
+                <div class="col-8"></div>
+                <div class="col-4">
+                  <button class="btn btn-primary btn-block" onClick={hai}>
+                    Sign In
+                  </button>
                 </div>
-              </form>
+              </div>
               <p class="mb-1">
                 <br></br>
                 <a href="forgot-password.html">I forgot my password</a>
@@ -79,6 +98,6 @@ function App() {
       </div>
     </div>
   );
-}
+};
 
 export default App;
