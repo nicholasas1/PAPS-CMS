9ae395c5382a91a73897379713795c1812fa74f2
nicholasas1 <nicholasantonius46@gmail.com>
2022-08-19 14:36:13 +0700

    Fixing sidebar and navbar

--- src/App.js
+++ src/App.js
@@ -28,76 +28,85 @@ const App = () => {
       toast("Login Gagal");
     } else if (e.status == "succes") {
       if (e.result[0].Role == "Admin") {
+        sessionStorage.setItem("Login", "success");
+        sessionStorage.setItem("Username", e.result[0].Username);
+        sessionStorage.setItem("Profile_picture", e.result[0].Profile_picture);
+        sessionStorage.setItem("First_name", e.result[0].First_name);
+        sessionStorage.setItem("Bidang", e.result[0].Bidang);
         window.location.href = "/dashboard";
       } else {
         toast("Hanya admin yang dapat login");
       }
     }
   };
-  return (
-    <div className="App">
-      <ToastContainer />
-      <div class="hold-transition login-page">
-        <div class="login-box">
-          <div class="login-logo">
-            <a href="../../index2.html">
-              <b>My</b>Akolit
-            </a>
-          </div>
-          <div class="card">
-            <div class="card-body login-card-body">
-              <p class="login-box-msg">Sign in to start your session</p>
-              <div class="input-group mb-3">
-                <input
-                  type="username"
-                  class="form-control"
-                  placeholder="Email"
-                  value={username}
-                  onChange={handleChange}
-                />
-                <div class="input-group-append">
-                  <div class="input-group-text">
-                    <span class="fas fa-envelope"></span>
+  if (sessionStorage.getItem("Login") == "success") {
+    window.location.href = "/dashboard";
+  } else {
+    return (
+      <div className="App">
+        <ToastContainer />
+        <div class="hold-transition login-page">
+          <div class="login-box">
+            <div class="login-logo">
+              <a href="../../index2.html">
+                <b>My</b>Akolit
+              </a>
+            </div>
+            <div class="card">
+              <div class="card-body login-card-body">
+                <p class="login-box-msg">Sign in to start your session</p>
+                <div class="input-group mb-3">
+                  <input
+                    type="username"
+                    class="form-control"
+                    placeholder="Email"
+                    value={username}
+                    onChange={handleChange}
+                  />
+                  <div class="input-group-append">
+                    <div class="input-group-text">
+                      <span class="fas fa-envelope"></span>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div class="input-group mb-3">
-                <input
-                  type="password"
-                  class="form-control"
-                  placeholder="Password"
-                  value={password}
-                  onChange={handleChange2}
-                />
-                <div class="input-group-append">
-                  <div class="input-group-text">
-                    <span class="fas fa-lock"></span>
+                <div class="input-group mb-3">
+                  <input
+                    type="password"
+                    class="form-control"
+                    placeholder="Password"
+                    value={password}
+                    onChange={handleChange2}
+                  />
+                  <div class="input-group-append">
+                    <div class="input-group-text">
+                      <span class="fas fa-lock"></span>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div class="row">
-                <div class="col-8"></div>
-                <div class="col-4">
-                  <button class="btn btn-primary btn-block" onClick={hai}>
-                    Sign In
-                  </button>
+                <div class="row">
+                  <div class="col-8"></div>
+                  <div class="col-4">
+                    <button class="btn btn-primary btn-block" onClick={hai}>
+                      Sign In
+                    </button>
+                  </div>
                 </div>
+                <p class="mb-1">
+                  <br></br>
+                  <a href="forgot-password.html">I forgot my password</a>
+                </p>
+                <p class="mb-0">
+                  <a href="register.html" class="text-center">
+                    Register a new membership
+                  </a>
+                </p>
               </div>
-              <p class="mb-1">
-                <br></br>
-                <a href="forgot-password.html">I forgot my password</a>
-              </p>
-              <p class="mb-0">
-                <a href="register.html" class="text-center">
-                  Register a new membership
-                </a>
-              </p>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default App;
--- src/Include/sidebar.js
+++ src/Include/sidebar.js
@@ -5,26 +5,11 @@ function Sidebar() {
   return (
     <div className="App">
       <aside class="main-sidebar sidebar-dark-primary elevation-4">
-        <a href="index3.html" class="brand-link">
-          <span class="brand-text font-weight-light">AdminLTE 3</span>
+        <a href="/" class="brand-link">
+          <span class="brand-text font-weight-light">My Akolit</span>
         </a>
 
         <div class="sidebar">
-          <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-            <div class="image">
-              <img
-                src="dist/img/user2-160x160.jpg"
-                class="img-circle elevation-2"
-                alt="User Image"
-              />
-            </div>
-            <div class="info">
-              <a href="#" class="d-block">
-                Alexander Pierce
-              </a>
-            </div>
-          </div>
-
           <div class="form-inline">
             <div class="input-group" data-widget="sidebar-search">
               <input
